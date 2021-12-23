903bffe68a4e66059375d8cf117d76052913a998
Noah Overcash <me@ncovercash.dev>
2021-12-23 01:18:38 -0500

    Add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      heading: `"Averia Sans Libre", sans, sans-serif`,
+      round: `"M PLUS Rounded 1c", sans, sans-serif`,
+    },
   },
   plugins: [],
 };
